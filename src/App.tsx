3b0827b53ff7a2b10985d4f1ae6805c80f733733
lunasky-hy <19776220+lunasky-hy@users.noreply.github.com>
2025-08-22 09:38:00 +0900

    first commit & apply tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>hoge</h1>
+      <p className="text-blue-600 font-bold text-5xl">
+        Hello Tailwind
+      </p>
+    </>
+  )
+}
+
+export default App
